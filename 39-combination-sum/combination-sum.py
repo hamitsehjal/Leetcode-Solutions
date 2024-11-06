31cbc9c30b193293d361cd75dbf8a7dcac140484
Hamit Sehjal <hamitsehjal26@gmail.com>
2024-11-06 14:51:55 -0500

    Time: 11 ms (63.82%) | Memory: 16.8 MB (32.54%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
+        
         def dfs(i,comb,target):
             if target == 0:
                 ans.append(comb.copy())
@@ -9,19 +9,14 @@ class Solution:
             if target < 0 or i >= len(candidates):
                 return
             
-            for j in range(i,len(candidates)):
-                elem = candidates[j]
-                if elem > target:
-                    return
-                comb.append(elem)
-                dfs(j,comb,target-elem)
-                comb.pop()
+            elem = candidates[i]
+            comb.append(elem)
 
-        candidates.sort()
-        ans = []
+            dfs(i,comb,target-elem)
 
-        dfs(0,[],target)
-
-        return ans
-
+            comb.pop()
+            dfs(i+1,comb,target)
         
+        ans = []
+        dfs(0,[],target)
+        return ans
