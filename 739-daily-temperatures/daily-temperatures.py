a6d9b36ab8be5aae573ec4c93cf2779818b174ed
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-02-12 21:06:13 -0500

    Time: 92 ms (71.86%) | Memory: 27.6 MB (48.28%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,13 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        ans = [0] * len(temperatures)
-        stack = []  # monotonic non-increasing stack
+        nextWarmer = [0]*len(temperatures)
+        stack = [] # monotonic non-increasing stack
 
         for i in range(len(temperatures)):
             while stack and temperatures[stack[-1]] < temperatures[i]:
                 stack_top = stack.pop()
-                ans[stack_top] = i - stack_top
-
+                nextWarmer[stack_top] = i-stack_top
+            
             stack.append(i)
+        
+        return nextWarmer
 
-        return ans
+        
