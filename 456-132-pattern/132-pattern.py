e0b41aaf1d1b5813b4792c57a361bc0683470f72
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-02-12 22:51:34 -0500

    Time: 91 ms (56.39%) | Memory: 34.9 MB (89.73%) - LeetSync

--- 456-132-pattern/132-pattern.py
+++ 456-132-pattern/132-pattern.py
@@ -1,39 +1,33 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         '''
-        subsequence: sequence derived by deleting some or no element from original sequence while keeping the order of remainaing elements intact
-
-        i < j < k
-        nums[i] < nums[k] < nums[k]
-
-
-        For each element, find
-        - Previous greater element of k
-        - For this previous greater element, find its previous minimum eleement
-        - If this previous minimum element is less than current element, we found 132 pattern
+         i < j < k
+         where
+         nums[i] < nums[k] < nums[j]
+         1 3 2
         '''
 
-        minimums = [0] * len(nums)
-        stack = []
+        minimums = [-1]*len(nums)
+        stack = [] # monotonic strictly decreasing
 
         for i in range(len(nums)):
             if i == 0:
-                minimums[0] = 0
+                minimums[i] = 0
             else:
                 if nums[i] < nums[minimums[i-1]]:
                     minimums[i] = i
                 else:
                     minimums[i] = minimums[i-1]
-            
+
             while stack and nums[stack[-1]] <= nums[i]:
                 stack.pop()
             
             if stack:
                 if nums[minimums[stack[-1]]] < nums[i]:
                     return True
-
+            
             stack.append(i)
         
-
         return False
+
         
