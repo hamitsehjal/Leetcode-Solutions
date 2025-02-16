08bb93193084ed6794451da36d599843a9d2b875
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-02-15 19:41:31 -0500

    Time: 3 ms (91.38%) | Memory: 21.2 MB (62.15%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,34 @@
-import heapq as hq
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        frequency = Counter(nums) # key(number) -> value(count)
-        minHeap = []
+        """
+        [1,1,1,2,2,3], k = 2
 
-        for key,val in frequency.items():
-            hq.heappush(minHeap,(val,key))
-            
-            if len(minHeap) > k:
-                hq.heappop(minHeap)
+        1. Build a hashmap mapping element to its frequency
+            O(N)
+            {
+                1 -> 3
+                2 -> 2
+                3 -> 1
+            }
+
+        2. Convert the hashmap into a list of pairs (integer,frequency)
+        [(1,3),(2,2),(3,1)] - O(n)
+
+        3. Sort the above array based on the frequency in reverse order
+        [(1,3),(2,2),(3,1)] - O(n logn)
+
+        4. Build up the resultant array - O(k)
+        """
+
+        frequency = defaultdict(int)
+        for num in nums:
+            frequency[num] += 1
+
+        pairs = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
+
+        ans = []
+        for i in range(k):
+            ans.append(pairs[i][0])
         
-        return [key for val,key in minHeap]
+        return ans
 
-            
-
-        
-
-        
