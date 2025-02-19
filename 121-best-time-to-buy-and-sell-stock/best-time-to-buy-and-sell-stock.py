c3c98f5e41913f8de2ea9a40e1c4f1500a764353
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-02-18 23:47:11 -0500

    Time: 99 ms (49.06%) | Memory: 26.7 MB (99.34%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        [7,1,5,3,6,4]
+
+        We start at 7,
+        - if we come across larger value, that means we can make profit
+        - if we come across smaller value, this would be the new buying point
+        """
+        if len(prices) == 1:
+            return 0
+        buying = profit = 0
+
+        for selling in range(1, len(prices)):
+            if prices[selling] < prices[buying]:
+                buying = selling
+            profit = max(profit, prices[selling] - prices[buying])
+
+        return profit
