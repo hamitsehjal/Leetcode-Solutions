922687cd1f9f4bae454c43763c6a8c67718f675b
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-02-23 15:07:54 -0500

    Time: 10 ms (77.13%) | Memory: 19.5 MB (20.32%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,19 +1,18 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        stack = []  # monotonic strictly decreasing stack
-        maxArea = 0
-
+        stack = []  # non-increasing stack
+        area = 0
         for i in range(len(height)):
-            while stack and height[stack[-1]] <= height[i]:
+            while stack and height[stack[-1]] < height[i]:
                 stack_top = stack.pop()
-
                 if stack:
-                    # stack[-1] represent the previous greater element for stack_top
-                    # i represent the next greater element for stack_top
                     h = min(height[i], height[stack[-1]]) - height[stack_top]
                     w = i - (stack[-1] + 1)
-                    maxArea = maxArea + (w*h)
+
+                    area = area + (w*h)
                 
+            
             stack.append(i)
         
-        return maxArea
+
+        return area
