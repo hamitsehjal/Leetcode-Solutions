29ef1247d4e9a8e7bec828f006c5624aff58cb81
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-02-23 14:20:55 -0500

    Time: 21 ms (74.73%) | Memory: 19.5 MB (62.00%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,25 +1,14 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        stack = [] # monotonic non-increasing stack
+        next_greater = [-1] * len(nums)
 
-        stack = []
-        n = len(nums)
-        nextGreater = [-1] * n
-
-        # for j in range(2 * n):
-        #     i = j % n
-        #     while stack and nums[stack[-1]] < nums[i]:
-        #         stack_pop = stack.pop()
-        #         nextGreater[stack_pop] = nums[i]
-
-        #     stack.append(i)
+        for i in range(len(nums)*2):
+            i = i % len(nums)
+            while stack and nums[stack[-1]] < nums[i]:
+                stack_top = stack.pop()
+                next_greater[stack_top] = nums[i]
+            
+            stack.append(i)
         
-        for _ in range(2):
-            for i in range(n):
-                while stack and nums[stack[-1]] < nums[i]:
-                    stack_pop = stack.pop()
-                    nextGreater[stack_pop] = nums[i]
-
-                stack.append(i)
-
-
-        return nextGreater
+        return next_greater
