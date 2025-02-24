583c2284d3495003197b66da5f24bd253da27f9f
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-02-23 21:32:21 -0500

    Time: 172 ms (37.90%) | Memory: 36.3 MB (15.49%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,5 +1,6 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0)
         stack = [] # monotonic increasing stack
         max_area = 0
 
@@ -13,7 +14,4 @@ class Solution:
 
             stack.append((start,height))
         
-        while stack:
-            idx,h = stack.pop()
-            max_area = max(max_area,h*(len(heights)-idx))
         return max_area
