c82df6b3794df5e742d653222e0cdc37c0dadace
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-02-26 08:35:19 -0500

    Time: 15 ms (54.73%) | Memory: 19.1 MB (93.19%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,29 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        """
-        We compute the max height on left and right for each bar
-        Then, we can simply do min(maxLeft,maxRight) - bar's height
-        If it's more than zero, that means water can by trapped othewise water cannot be trapped at that location
-        """
-        
-        n = len(height)
+        l,r = 0,len(height)-1
+        max_left,max_right = height[l],height[r]
 
-        max_left = [0] * n
-        max_right = [0] * n
-
-        max_right[n-1] = 0
-
-        for i in range(1,n):
-            max_left[i] = max(max_left[i-1],height[i-1])
-        
-        for i in range(n-2,-1,-1):
-            max_right[i] = max(max_right[i+1],height[i+1])
-        
         res = 0
-        for i in range(n):
-            area = min(max_left[i],max_right[i]) - height[i]
-            if area > 0:
-                res += area
+
+        while l < r:
+            if max_left < max_right:
+                l += 1
+                max_left = max(max_left,height[l])
+                res += max_left - height[l]
+            else:
+                r -= 1
+                max_right = max(max_right,height[r])
+                res += max_right - height[r]
         
         return res
-
+        
