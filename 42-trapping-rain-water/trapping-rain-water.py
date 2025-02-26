2655b8941da9b9f7433f63f7d5d2175a9f591c8c
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-02-26 08:03:51 -0500

    Time: 22 ms (37.07%) | Memory: 19.2 MB (93.19%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,18 +1,29 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        stack = []  # non-decreasing stack
-        area = 0
-        for i in range(len(height)):
-            while stack and height[stack[-1]] <= height[i]:
-                stack_top = stack.pop()
-                if stack:
-                    # stack[-1] top of the stack is our left bounary
-                    # stack_top (popped off value) is our gap
-                    # height[i] current element is our right boundary
-                    h = min(height[i], height[stack[-1]]) - height[stack_top]
-                    w = i - (stack[-1] + 1)
-                    area = area + (w * h)
+        """
+        We compute the max height on left and right for each bar
+        Then, we can simply do min(maxLeft,maxRight) - bar's height
+        If it's more than zero, that means water can by trapped othewise water cannot be trapped at that location
+        """
+        
+        n = len(height)
 
-            stack.append(i)
+        max_left = [0] * n
+        max_right = [0] * n
 
-        return area
+        max_right[n-1] = 0
+
+        for i in range(1,n):
+            max_left[i] = max(max_left[i-1],height[i-1])
+        
+        for i in range(n-2,-1,-1):
+            max_right[i] = max(max_right[i+1],height[i+1])
+        
+        res = 0
+        for i in range(n):
+            area = min(max_left[i],max_right[i]) - height[i]
+            if area > 0:
+                res += area
+        
+        return res
+
