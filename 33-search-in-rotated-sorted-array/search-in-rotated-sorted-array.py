fab4b1caf91cfd1b039823d3d94990dc0e5da0ca
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-03-11 23:41:56 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (67.65%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,25 +1,46 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # find the pivot index first
+        l,r = 0,len(nums)-1
 
-        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = l + (r-l) // 2
 
-        while l <= r:
-            mid = (l + r) // 2
-
-            if nums[mid] == target:
-                return mid
-            elif nums[l] <= nums[mid]:
-                # l -> mid is sorted
-                if target < nums[l] or target > nums[mid]:
-                    # not in this set
-                    l = mid + 1
-                else:
-                    r = mid - 1
+            if nums[mid] <= nums[-1]:
+                r = mid
             else:
-                # mid + 1 -> r is sorted
-                if target < nums[mid+1] or target > nums[r]:
+                l = mid + 1
+        
+        pivot = l
+
+        # decide on range based on pivot
+        if nums[0] <= target <= nums[pivot-1]:
+            # left range
+            l,r = 0,pivot-1
+
+            while l <= r:
+                mid = l + (r-l)//2
+
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
                     r = mid - 1
                 else:
                     l = mid + 1
+        
+        if nums[pivot] <= target <= nums[-1]:
+            # right range
+            l,r = pivot,len(nums)-1
 
+            while l <= r:
+                mid = l + (r-l)//2
+
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        
         return -1
+        
