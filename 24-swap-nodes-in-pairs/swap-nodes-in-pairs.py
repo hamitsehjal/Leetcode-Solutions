fe12dafcb8fc44368108ea58719832bcf71fe475
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-03-18 20:45:11 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.18%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -1,17 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0, head)
-        prev, cur = dummy, head
+        dummy = ListNode(0,head)
+        cur,prev = head,dummy
 
+        # we need atleast two pairs to reverse
         while cur and cur.next:
-            npn = cur.next.next
+
+            # save ptrs
+            nextPair = cur.next.next
             second = cur.next
 
+            # reverse the pair
             second.next = cur
-            cur.next = npn
+            cur.next = nextPair
             prev.next = second
 
+            # update the pairs
             prev = cur
-            cur = npn
-        
+            cur = nextPair 
+
         return dummy.next
+        
+        
