a3bee428cc570243cb1a4f9305b3aa5366adafa4
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-03-25 14:14:41 -0400

    Time: 246 ms (19.71%) | Memory: 38.3 MB (56.33%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,13 +1,22 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [(p, s) for p, s in zip(position, speed)]
-        pair.sort(reverse=True)
+        pairs = [[p,s] for p,s in zip(position,speed)]
+        pairs = sorted(pairs,reverse=True)
 
-        stack = []  # next smaller or equal -> monotonic strictly increasing stack
-        for pos, spd in pair:
-            arrival_time = (target - pos) / spd
+        stack = [] # strictly increasing stack
+
+        for i in range(len(pairs)):
+            p,s = pairs[i]
+            arrival_time = (target - p)/s
+
             if stack and stack[-1] >= arrival_time:
                 continue
+            
             stack.append(arrival_time)
-
+        
         return len(stack)
+            
+
+        
+
+    
