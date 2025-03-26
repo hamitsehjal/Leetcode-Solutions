d496286f3ad4864aff5f3197ce5f518b3238d81f
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-03-26 00:52:46 -0400

    Time: 88 ms (53.33%) | Memory: 35 MB (83.28%) - LeetSync

--- 456-132-pattern/132-pattern.py
+++ 456-132-pattern/132-pattern.py
@@ -1,41 +1,24 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        """
-        nums = [3,1,4,2]
-
-        i < j < k
-        nums[i] < nums[k] < nums[j]
-
-        for the current element (k)
-        - find its previous greater element (j)
-        - for this previous greater element, find the minimum element(i)
-        - check if this minimum element (i) is < current elememnt(k)
-        """
-
-        stack = [] # monotonic strictly decreasing 
-        minimums = [0]*len(nums) # index of minimum in the range(0-i)
+        stack = [] # strictly decreasing monotonic stack
+        minimum = [-1] * len(nums)
 
         for i in range(len(nums)):
             if i == 0:
-                minimums[0] == 0
+                minimum[0] = nums[0]
             else:
-                if nums[i] < nums[minimums[i-1]]:
-                    minimums[i] = i
+                if nums[i] < minimum[i-1]:
+                    minimum[i] = nums[i]
                 else:
-                    minimums[i] = minimums[i-1]
-
+                    minimum[i] = minimum[i-1]
+            
             while stack and nums[stack[-1]] <= nums[i]:
                 stack.pop()
             
             if stack:
-                previous_greater_idx = stack[-1]
-                minimum_element = nums[minimums[previous_greater_idx]]
-                if minimum_element < nums[i]:
+                if minimum[stack[-1]] < nums[i]:
                     return True
-                
-            
+
             stack.append(i)
         
         return False
-
-
