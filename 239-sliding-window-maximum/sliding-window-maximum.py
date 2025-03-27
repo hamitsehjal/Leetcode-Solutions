7461c5429d1535727a214864a98631e189b17e3a
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-03-27 17:30:22 -0400

    Time: 157 ms (74.59%) | Memory: 31.3 MB (63.89%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -4,16 +4,17 @@ class Solution:
         result = []
 
         for i in range(len(nums)):
-            while deque and deque[0] == i-k:
-                deque.popleft()
-
             while deque and nums[deque[-1]] < nums[i]:
                 deque.pop()
             
             deque.append(i)
 
+
+            if deque[0] + k == i:
+                deque.popleft()
+
             if i >= k-1:
                 result.append(nums[deque[0]])
-        
+
         return result
         
