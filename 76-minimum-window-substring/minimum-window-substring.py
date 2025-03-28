64ab2187fb407f14f441313a0f5af83a9a063264
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-03-28 13:56:38 -0400

    Time: 116 ms (42.23%) | Memory: 18.3 MB (55.79%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -3,36 +3,33 @@ class Solution:
         if len(s) < len(t):
             return ""
 
-        """
-        Confusion??
-        1. when does the window becomes invalid?
-        2. How to make the work inside the for loop - O(1)
-        """
+        t_map, s_map = {}, {}
 
-        freq_t, freq_s = {}, {}
         for ch in t:
-            freq_t[ch] = 1 + freq_t.get(ch, 0)
+            t_map[ch] = 1 + t_map.get(ch, 0)
 
-        need, have = len(freq_t), 0
-
+        need, have = len(t_map), 0
         l = 0
-        res, res_len = "", float("inf")
 
+        res, res_len = "", float("inf")
         for r in range(len(s)):
             ch = s[r]
-            freq_s[ch] = 1 + freq_s.get(ch, 0)
+            s_map[ch] = 1 + s_map.get(ch, 0)
 
-            if ch in freq_t and freq_t[ch] == freq_s[ch]:
+            if ch in t_map and t_map[ch] == s_map[ch]:
                 have += 1
 
             while have == need:
                 if (r - l + 1) < res_len:
-                    res_len = r - l + 1
                     res = s[l : r + 1]
-                ch = s[l]
-                freq_s[ch] -= 1
-                if ch in freq_t and freq_t[ch] > freq_s[ch]:
-                    have -= 1
-                l += 1
+                    res_len = r - l + 1
 
+                ch = s[l]
+                s_map[ch] = s_map[ch] - 1
+
+                if ch in t_map and t_map[ch] > s_map[ch]:
+                    have -= 1
+
+                l += 1
+                
         return res
