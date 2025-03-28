4330acc7122a089d003f6afb1016f5f97241c212
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-03-28 15:10:28 -0400

    Time: 239 ms (20.01%) | Memory: 18.1 MB (19.57%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,52 +1,36 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        """
-        Comparing all permutations ❌ O(N!)
-        hashmap of the s1 ✅ O(N^2)
-
-        Fixed window question ✅
-
-        Fair solution - comparing hashmpas - O(26 * N)
-        - 26 since hash map cannot outgrow this based on question constraints
-
-        Optimal Sliding window question
-        - a really good trick (Coding)
-        """
-
         if len(s1) > len(s2):
-            # there is no way we could find a permutation that is lareger than the size of s2
             return False
 
-        s1_count, s2_count = [0] * 26, [0] * 26
+        s1_map, s2_map = {}, {}
+
+        for ch in s1:
+            s1_map[ch] = 1 + s1_map.get(ch, 0)
 
         for i in range(len(s1)):
-            s1_count[ord(s1[i]) - ord("a")] += 1
-            s2_count[ord(s2[i]) - ord("a")] += 1
+            ch = s2[i]
+            s2_map[ch] = 1 + s2_map.get(ch, 0)
 
-        matches = sum([1 if s1_count[i] == s2_count[i] else 0 for i in range(26)])
+        if s2_map == s1_map:
+            return True
 
+        print(s1_map)
+        print(s2_map)
         l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
-                return True
+        for r in range(len(s1),len(s2)):
+            ch = s2[r]
+            s2_map[ch] = 1 + s2_map.get(ch, 0)
 
-            index = ord(s2[r]) - ord("a")
-            s2_count[index] += 1
-
-            if s1_count[index] == s2_count[index]:
-                matches += 1
-
-            elif s1_count[index] + 1 == s2_count[index]:
-                matches -= 1
-
-            index = ord(s2[l]) - ord("a")
-            s2_count[index] -= 1
-
-            if s1_count[index] == s2_count[index]:
-                matches += 1
-            elif s1_count[index] - 1 == s2_count[index]:
-                matches -= 1
+            ch = s2[l]
+            s2_map[ch] -= 1
+            if s2_map[ch] == 0:
+                del s2_map[ch]
 
             l += 1
+            print(f" {l} | {r} | {s1_map}")
+            print(f" {l} | {r} | {s2_map}")
+            if s2_map == s1_map:
+                return True
 
-        return matches == 26
+        return False
