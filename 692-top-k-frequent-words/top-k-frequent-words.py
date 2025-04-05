3e3f34a152d71708481ecb1a9441a8052c02b190
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-04-05 13:53:23 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (63.99%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,10 +1,19 @@
 from collections import Counter
-
+from heapq import heappush, heappop
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         freq = Counter(words)
-
-        freq_sorted = sorted(freq.items(), key=lambda x: (-x[1], x[0]))
-
-        return [freq_sorted[i][0] for i in range(k)]
+        heap = []
+        
+        # Push all words with negated frequency
+        for word, cnt in freq.items():
+            heappush(heap, (-cnt, word))
+        
+        # Pop the top k elements
+        ans = []
+        for _ in range(k):
+            _, word = heappop(heap)
+            ans.append(word)
+        
+        return ans
