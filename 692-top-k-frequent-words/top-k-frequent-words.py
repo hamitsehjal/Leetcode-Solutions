3c275645f617b89b2e43027053351058af14a107
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-04-05 14:40:32 -0400

    Time: 4 ms (22.43%) | Memory: 17.9 MB (63.99%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,34 +1,34 @@
 from collections import Counter
-from heapq import heappush, heappop
+from heapq import heappush,heappop
 
-
-class WordNode:
-    def __init__(self, freq: int, word: str):
-        self.freq = freq
+class Node:
+    def __init__(self,count,word):
+        self.count = count
         self.word = word
-
-    def __lt__(self, other):
-        if self.freq != other.freq:
-            return self.freq < other.freq
-
+    
+    def __lt__(self,other):
+        if self.count != other.count:
+            return self.count < other.count
+        
         return self.word > other.word
-
+        
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        freq = Counter(words)
+        counter = Counter(words)
+        heap = [] # min-heap
 
-        heap = []  # min-heap(pop off the ones with less freq)
-        for word, cnt in freq.items():
-            heappush(heap, WordNode(cnt, word))
+        for word,count in counter.items():
+            heappush(heap,Node(count,word))
 
             if len(heap) > k:
                 heappop(heap)
-
+        
         ans = []
-
         while heap:
             node = heappop(heap)
             ans.append(node.word)
-
+        
         return ans[::-1]
+
+        
