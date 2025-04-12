00e2f35c1cb8b14143b8fb754ef2bd30c0830ee6
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-04-12 18:04:31 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.54%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        number = str(num)
+        cur = num
+        for i in range(len(number)):
+            if number[i] == "6":
+                new_version = number[:i] + "9" + number[i + 1 :]
+                cur = max(cur, int(new_version))
+        print(cur)
+        return cur
