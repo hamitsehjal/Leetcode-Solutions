6442b38f6b21f504a076550a7fa27534398d33f7
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-04-22 23:37:01 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (68.76%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,33 +1,31 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
         if len(digits) == 0:
             return []
-            
-        lettersMap = {
+
+        letterMap = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
             "5": "jkl",
             "6": "mno",
-            "7": "qprs",
+            "7": "pqrs",
             "8": "tuv",
             "9": "wxyz",
         }
-        ans = []
-        def backtrack(i,comb):
-            if len(comb) == len(digits):
-                ans.append("".join(comb.copy()))
+
+        def backtrack(start, cur):
+            if len(cur) == len(digits):
+                ans.append("".join((cur[:])))
                 return
-            
-            digit = digits[i]
-            for letter in lettersMap[digit]:
-                comb.append(letter)
-                backtrack(i+1,comb)
-                comb.pop()
-        
-        backtrack(0,[])
+
+            for i in range(start, len(digits)):
+                digit = digits[i]
+                for letter in letterMap[digit]:
+                    cur.append(letter)
+                    backtrack(i + 1, cur)
+                    cur.pop()
+
+        ans = []
+        backtrack(0, [])
         return ans
-
-            
-
