bb36a4352fec37876b9287507054d7007907d377
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-04-28 22:26:47 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (87.04%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+
+        def backtrack(idx, comb, curSum):
+            if len(comb) == k and curSum == n:
+                ans.append(comb[:])
+                return
+
+            if curSum > n or len(comb) > k:
+                return
+
+            for num in range(idx, 10):
+                if curSum+num <= n:
+                    comb.append(num)
+                    print(f"bactrack({idx+1},{comb},{curSum+num})")
+                    backtrack(num + 1, comb, curSum + num)
+                    comb.pop()
+
+        backtrack(1, [], 0)
+        return ans
+        
