3575fa638fbf5494b9a86b348fc1c3a364fc4192
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-05-06 21:24:49 -0400

    Time: 407 ms (37.62%) | Memory: 25.7 MB (24.63%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,36 +1,32 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        '''
-        state = (node + removals)
-        '''
-        def isValid(r,c):
-            return 0 <= r < m and 0 <= c < n
+        """
+        BFS visits a state in the minimum distance possible
+        State is not always (r,c)
+        """
+        m, n = len(grid), len(grid[0])
 
-        queue=deque([(0,0,0,k)]) # row,col,steps,removals
-        visit={(0,0,k)} # row,col,removals
-
-        directions=[(0,-1),(0,1),(-1,0),(1,0)]
-        m=len(grid)
-        n=len(grid[0])
+        queue = collections.deque([(0, 0, 0, k)])
+        visited = {(0, 0, k)}
 
         while queue:
-            row,col,steps,removals = queue.popleft()
-
-            if row == m-1 and col == n-1:
+            row, col, steps, removals = queue.popleft()
+            if row == m - 1 and col == n - 1:
                 return steps
-            
-            for dr,dc in directions:
-                nxt_row,nxt_col = row+dr,col+dc
-                if isValid(nxt_row,nxt_col):
-                    if grid[nxt_row][nxt_col] == 1:
-                        # obstacle
-                        if removals and (nxt_row,nxt_col,removals-1) not in visit:
-                            visit.add((nxt_row,nxt_col,removals-1))
-                            queue.append((nxt_row,nxt_col,steps+1,removals-1))
+
+            for dr, dc in [[0, 1], [0, -1], [-1, 0], [1, 0]]:
+                r, c = row + dr, col + dc
+
+                if r >= 0 and r < m and c >= 0 and c < n:
+                    # in bounds
+                    if grid[r][c] == 1:
+                        if removals > 0 and (r, c, removals - 1) not in visited:
+                            visited.add((r, c, removals - 1))
+                            queue.append((r, c, steps + 1, removals - 1))
                     else:
-                        # no obstacle
-                        if (nxt_row,nxt_col,removals) not in visit:
-                            visit.add((nxt_row,nxt_col,removals))
-                            queue.append((nxt_row,nxt_col,steps+1,removals))     
+                        # not an obstacle
+                        if (r, c, removals) not in visited:
+                            visited.add((r, c, removals))
+                            queue.append((r, c, steps + 1, removals))
+
         return -1
-            
