b9cbc97e8b84aa647f54be909f59461d3f828a36
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-05-21 17:03:18 -0400

    Time: 7 ms (97.43%) | Memory: 18.1 MB (58.97%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -5,39 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-
-    def dfs(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root and not subRoot:
-            return True
-        if not root or not subRoot:
-            return False
-        if root.val != subRoot.val:
-            return False
-
-        isLeftSame = self.dfs(root.left, subRoot.left)
-        isRightSame = self.dfs(root.right, subRoot.right)
-
-        return isLeftSame and isRightSame
-
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root and not subRoot:
-            return True
+        def serialize(node):
+            if not node:
+                return 'N'
+            
+            return f"({node.val},{serialize(node.left)},{serialize(node.right)})"
+        
+        rootString = serialize(root)
+        subRootString = serialize(subRoot)
 
-        if not root or not subRoot:
-            return False
-
-        queue = collections.deque([root])
-
-        while queue:
-            node = queue.popleft()
-            if node.val == subRoot.val:
-                # potential match, check left and right subtree
-                if self.dfs(node, subRoot):
-                    return True
-
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-
-        return False
+        return subRootString in rootString
+        
