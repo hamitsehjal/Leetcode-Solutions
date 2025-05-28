ca4f0cfd22576384f47c56eb4aa3ac2b4f0d0c4f
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-05-28 18:05:24 -0400

    Time: 2394 ms (28.88%) | Memory: 79.3 MB (82.75%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -3,15 +3,6 @@ import heapq
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        graph = {i: [] for i in range(len(points))}
-
-        for i in range(len(points)):
-            for j in range(i + 1, len(points)):
-                x1, y1 = points[i]
-                x2, y2 = points[j]
-                d = abs(y2 - y1) + abs(x2 - x1)
-                graph[i].append([j, d])  # edge, distance
-                graph[j].append([i, d])
 
         """
         # Prim's Algorithm
@@ -19,24 +10,22 @@ class Solution:
         2. Instantiate a set/array to track the visited nodes
         """
         visited = [False] * len(points)
-        minHeap = [(0, 0, -1)]  # weight,node,parent
-        mst = []  # edges of minimum spanning tree
-        total_weight = 0
+        pq = [(0, 0)]  # weight,node,parent
+        min_cost = 0
 
-        while minHeap and len(mst) < len(points) - 1:
-            weight, node, parent = heapq.heappop(minHeap)
+        while pq:
+            weight, u = heapq.heappop(pq)
 
-            if visited[node]:
+            if visited[u]:
                 continue
 
-            visited[node] = True
-            if parent != -1:
-                mst.append((parent, node, weight))  # parent,node,weight
-                total_weight += weight
+            visited[u] = True
+            min_cost += weight
 
-            for nei, wei in graph[node]:
-                if visited[nei]:
-                    continue
-                heapq.heappush(minHeap, (wei, nei, node))
+            
+            for v in range(len(points)):
+                if not visited[v]:
+                    dist = abs(points[u][0]-points[v][0]) + abs(points[u][1] - points[v][1])
+                    heapq.heappush(pq,(dist,v))
 
-        return total_weight
+        return min_cost
