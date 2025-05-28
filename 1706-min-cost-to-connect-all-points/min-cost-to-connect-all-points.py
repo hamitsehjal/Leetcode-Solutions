ba51f41d17e3699175d9106322600bbc2da7359f
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-05-28 18:07:36 -0400

    Time: 505 ms (92.17%) | Memory: 21.7 MB (90.86%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -3,7 +3,6 @@ import heapq
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-
         """
         # Prim's Algorithm
         1. Instantiate a priority Queue with an aribitrary node as starting point
@@ -12,6 +11,7 @@ class Solution:
         visited = [False] * len(points)
         pq = [(0, 0)]  # weight,node,parent
         min_cost = 0
+        cache = {0: 0}  # map node to current minimum distance
 
         while pq:
             weight, u = heapq.heappop(pq)
@@ -22,10 +22,13 @@ class Solution:
             visited[u] = True
             min_cost += weight
 
-            
             for v in range(len(points)):
                 if not visited[v]:
-                    dist = abs(points[u][0]-points[v][0]) + abs(points[u][1] - points[v][1])
-                    heapq.heappush(pq,(dist,v))
+                    dist = abs(points[u][0] - points[v][0]) + abs(
+                        points[u][1] - points[v][1]
+                    )
+                    if dist < cache.get(v, float("inf")):
+                        cache[v] = dist
+                        heapq.heappush(pq, (dist, v))
 
         return min_cost
