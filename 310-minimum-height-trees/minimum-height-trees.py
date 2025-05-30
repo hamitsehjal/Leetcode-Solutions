d0526b0424b52fb9ac2635d8e0022fd8610bc906
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-05-30 15:44:59 -0400

    Time: 75 ms (26.75%) | Memory: 27.3 MB (51.12%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,30 +1,27 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-
         if n == 1:
             return [0]
-            
-        degrees = {i: 0 for i in range(n)}
+
+        indegrees = {u : 0 for u in range(n)}
         graph = defaultdict(list)
 
-        for u, v in edges:
+        for u,v in edges:
             graph[u].append(v)
             graph[v].append(u)
-            degrees[u] += 1
-            degrees[v] += 1
+            indegrees[u] += 1
+            indegrees[v] += 1
+        
+        leaves = deque([u for u in indegrees if indegrees[u] == 1])
 
-        queue = deque([u for u in degrees if degrees[u] == 1])
-        remaining_nodes = n
-        while remaining_nodes > 2:
-            level_size = len(queue)
-            remaining_nodes -= level_size
+        while leaves and n > 2:
 
-            for _ in range(level_size):
-                node = queue.popleft()
-
+            for _ in range(len(leaves)):
+                node = leaves.popleft()
+                n -= 1
                 for nei in graph[node]:
-                    degrees[nei] -= 1
-                    if degrees[nei] == 1:
-                        queue.append(nei)
-
-        return list(queue)
+                    indegrees[nei] -= 1
+                    if indegrees[nei] == 1:
+                        leaves.append(nei)
+        
+        return list(leaves)
