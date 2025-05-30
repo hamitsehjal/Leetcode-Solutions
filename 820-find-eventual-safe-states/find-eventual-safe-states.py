141e48bf28b264aae9f2df41ee3ab83be292e6c1
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-05-29 20:01:57 -0400

    Time: 103 ms (8.61%) | Memory: 24.1 MB (23.34%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,30 +1,46 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        reversedGraph = collections.defaultdict(list)
-        indegree = {}
-        for i in range(len(graph)):
-            indegree[i] = 0
+        """
+        Safety as a relationship
+        - a node is safe if all the nodes that it points to are safe
+        - 0 is safe, if 1,2 are safe
+            1 -> 0
+            2 -> 0
+        - 1 is safe, if 2,3 are safe
+            2 -> 1
+            3 -> 1
+        - 2 is safe, if 5 is safe
+            5 -> 2
+        - 3 is safe, if 0 is safe
+            0 -> 3
+        - 4 is safe, if 5 is safe
+            5 -> 4
+        - 5 is safe, no prerequiste
+        - 6 is safe, no prerequisite
 
-        for idx,pair in enumerate(graph):
-            for nei in pair:
-                reversedGraph[nei].append(idx)
-                indegree[idx] += 1
+        """
+        indegrees = {u: 0 for u in range(len(graph))}
+        neigbhours = defaultdict(list)
+
+        for u,dependencies in enumerate(graph):
+            for v in dependencies:
+                # dependency(v) -> dependent(u)
+                indegrees[u] += 1
+                neigbhours[v].append(u)
         
-
-        queue = collections.deque([node for node in indegree if indegree[node] == 0])
-
+        queue = deque([u for u in indegrees if indegrees[u] == 0])
         topo_order = []
 
         while queue:
             node = queue.popleft()
             topo_order.append(node)
-            for nei in reversedGraph[node]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
+            for nei in neigbhours[node]:
+                indegrees[nei] -= 1
+                if indegrees[nei] == 0:
                     queue.append(nei)
         
-
         return sorted(topo_order)
+
+
+
         
-
-
