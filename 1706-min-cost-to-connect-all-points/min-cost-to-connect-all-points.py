acec0aa72c1fd4a8cc97b7af73af72529f7d2fc3
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-06-03 20:01:50 -0400

    Time: 2253 ms (31.89%) | Memory: 79.5 MB (82.24%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,34 +1,22 @@
-import heapq
-
-
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        """
-        # Prim's Algorithm
-        1. Instantiate a priority Queue with an aribitrary node as starting point
-        2. Instantiate a set/array to track the visited nodes
-        """
-        visited = [False] * len(points)
-        pq = [(0, 0)]  # weight,node,parent
-        min_cost = 0
-        cache = {0: 0}  # map node to current minimum distance
+        pq = [(0,0)] # (weight,node)
+        n = len(points)
+        visited = [False] * n
+        total_cost = 0
+        mst = []
 
         while pq:
-            weight, u = heapq.heappop(pq)
+            weight,u = heapq.heappop(pq)
 
-            if visited[u]:
-                continue
-
-            visited[u] = True
-            min_cost += weight
-
-            for v in range(len(points)):
-                if not visited[v]:
-                    dist = abs(points[u][0] - points[v][0]) + abs(
-                        points[u][1] - points[v][1]
-                    )
-                    if dist < cache.get(v, float("inf")):
-                        cache[v] = dist
-                        heapq.heappush(pq, (dist, v))
-
-        return min_cost
+            if not visited[u]:
+                visited[u] = True
+                total_cost += weight
+                mst.append(u)
+            
+                for v,point in enumerate(points):
+                    if not visited[v]:
+                        distance = abs(point[0] - points[u][0]) + abs(point[1] - points[u][1])
+                        heapq.heappush(pq,(distance,v))
+        
+        return total_cost
