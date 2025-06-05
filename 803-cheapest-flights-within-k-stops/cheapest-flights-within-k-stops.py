7de45e72ebbf9963b8a692434c8af6bf7d4f6e1d
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-06-04 23:22:19 -0400

    Time: 154 ms (6.21%) | Memory: 18.9 MB (84.44%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,35 +1,20 @@
 class Solution:
-    def findCheapestPrice(
-        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
-    ) -> int:
-        graph = {}
-        for x, y, w in flights:
-            if x not in graph:
-                graph[x] = []
-            graph[x].append([y, w])
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        
+        dist = [float('inf')] * n
+        dist[src] = 0
 
-        heap = [(0, 0, src)]  # cost,stops,city
-        visited = {}  # map (city,stops) -> cost
-        while heap:
-            cost, stops, city = heapq.heappop(heap)
+        for _ in range(k+1):
+            temp = dist[:]
 
-            if city == dst:
-                return cost
-
-            if stops > k:
-                continue
-
-            # if we have visited this city with fewer stops at lower cost, then skip it
-            if (city, stops) in visited and visited[(city, stops)] <= cost:
-                continue
-
-            visited[(city,stops)] = cost
+            for u,v,w in flights:
+                if dist[u] != float('inf'):
+                    temp[v] = min(temp[v],dist[u] + w)
             
-            for pair in graph.get(city, []):
-                nei, weight = pair
-                updated_cost = cost + weight
-                updated_stops = stops + 1
+            dist = temp
+        
 
-                heapq.heappush(heap, (updated_cost, updated_stops, nei))
-
-        return -1
+        if dist[dst] == float('inf'):
+            return -1
+        
+        return dist[dst]
