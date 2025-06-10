25ef01875867127845b1c399ebd54f30436c7f53
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-06-10 17:20:28 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.66%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,21 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-
-        def dp(i):
-            """
-            return the maximum money you can rob upto and including house i
-            """
-            if i == 0:
-                return nums[0]
-            
-            if i == 1:
-                return max(nums[0],nums[1])
-            
-            if i in memo:
-                return memo[i]
-
-            memo[i] = max(nums[i] + dp(i-2),dp(i-1))
-            return memo[i]
         
-        memo = {}
-        return dp(len(nums)-1)
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, n):
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+
+        return dp[n - 1]
