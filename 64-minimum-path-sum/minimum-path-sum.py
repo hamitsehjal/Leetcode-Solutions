dba934b312464c8675acb739c33cbaa6f698c3fa
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-06-11 19:34:41 -0400

    Time: 18 ms (37.81%) | Memory: 20.3 MB (38.35%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        """
+        Observations
+        - non-negative numbers
+        - find a path with min total sum
+
+        dp[2][2] = min(dp[1][2],dp[2][1]) + grid[2][2]
+        """
+        m, n = len(grid), len(grid[0])
+        dp = [[0 for c in range(n)] for r in range(m)]
+        dp[0][0] = grid[0][0]
+
+        for r in range(m):
+            for c in range(n):
+                val_1, val_2 = -1, -1
+                if 0 <= r - 1 < m:
+                    val_1 = dp[r - 1][c]
+                if 0 <= c - 1 < n:
+                    val_2 = dp[r][c - 1]
+
+                if val_1 == -1 and val_2 == -1:
+                    dp[r][c] = grid[r][c]
+                elif val_1 == -1:
+                    dp[r][c] = val_2 + grid[r][c]
+                elif val_2 == -1:
+                    dp[r][c] = val_1 + grid[r][c]
+                else:
+                    dp[r][c] = min(val_1, val_2) + grid[r][c]
+
+        return dp[m - 1][n - 1]
