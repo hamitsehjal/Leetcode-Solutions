1bb1b575e4c5f897a83599bc4757b91a2a3ffdbd
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-06-11 16:13:00 -0400

    Time: 47 ms (23.59%) | Memory: 52.7 MB (89.42%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -2,17 +2,17 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
+var expect = function (val) {
     return {
-        toBe:(val2)=>{
-            if (val === val2){
+        toBe: function (val2) {
+            if (val === val2) {
                 return true
             }
             throw new Error("Not Equal")
         },
 
-        notToBe:(val2)=>{
-            if (val !== val2){
+        notToBe: function (val2) {
+            if (val !== val2) {
                 return true
             }
             throw new Error("Equal")
