c49f48c86cbcf73acdab804ce216d02427620759
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-06-11 16:27:38 -0400

    Time: 46 ms (28.65%) | Memory: 55 MB (6.14%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -4,10 +4,9 @@
  * @return {number[]}
  */
 var map = function (arr, fn) {
-    new_arr = [];
+    const new_arr = new Array(arr.length);
     for (let i = 0; i < arr.length; i++) {
-        const item = arr[i];
-        new_arr.push(fn(item, i))
+        new_arr[i] = fn(arr[i], i)
     }
 
     return new_arr
