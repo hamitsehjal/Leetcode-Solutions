320a236c655822b6aa87ec9b8008e6ce615e9a4f
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-06-11 19:38:00 -0400

    Time: 15 ms (58.47%) | Memory: 20.1 MB (50.42%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,28 +1,20 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        """
-        Observations
-        - non-negative numbers
-        - find a path with min total sum
-
-        dp[2][2] = min(dp[1][2],dp[2][1]) + grid[2][2]
-        """
         m, n = len(grid), len(grid[0])
-        dp = [[0 for c in range(n)] for r in range(m)]
+        dp = [[0 for _ in range(n)] for _ in range(m)]
         dp[0][0] = grid[0][0]
 
-        for r in range(m):
-            for c in range(n):
-                val_1, val_2 = float('inf'),float('inf')
+        # Fill first row
+        for c in range(1, n):
+            dp[0][c] = dp[0][c - 1] + grid[0][c]
 
-                if 0 <= r - 1 < m:
-                    val_1 = dp[r - 1][c]
-                if 0 <= c - 1 < n:
-                    val_2 = dp[r][c - 1]
+        # Fill first column
+        for r in range(1, m):
+            dp[r][0] = dp[r - 1][0] + grid[r][0]
 
-                if val_1 == float('inf') and val_2 == float('inf'):
-                    dp[r][c] = grid[r][c]
-                else:
-                    dp[r][c] = min(val_1, val_2) + grid[r][c]
+        # Fill the rest of the dp table
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = min(dp[r - 1][c], dp[r][c - 1]) + grid[r][c]
 
         return dp[m - 1][n - 1]
