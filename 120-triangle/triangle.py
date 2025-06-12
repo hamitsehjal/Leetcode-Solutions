d195d18e0d4f45859b48541d8c1560f629618eb4
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-06-12 18:40:12 -0400

    Time: 4 ms (53.94%) | Memory: 18.9 MB (76.67%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,32 +1,20 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         ROWS = len(triangle)
-        dp = [0] * ROWS
-        dp[0] = triangle[0][0]
-
 
         for r in range(1,ROWS):
             cols = len(triangle[r])
-            new_dp = [0] * cols
 
             for c in range(cols):
-                """
-                For each position, we can come from:
-                1. dp[c-1]: from upper left
-                2. dp[c]: direct above
-                """
+
                 if c == 0:
                     # leftmost position, can only come from direct obove
-                    new_dp[c] = dp[c]
+                    triangle[r][c] += triangle[r-1][c]
                 elif c == cols-1:
                     # rightmost position, can only come from upper left
-                    new_dp[c] = dp[c-1]
+                    triangle[r][c] += triangle[r-1][c-1]
                 else:
-                    new_dp[c] = min(dp[c],dp[c-1])
-                
-                new_dp[c] += triangle[r][c]
-            
-            dp = new_dp
-        
+                    triangle[r][c] += min(triangle[r-1][c],triangle[r-1][c-1])
 
-        return min(dp)
+
+        return min(triangle[-1])
