4b211c51e5580982011bf6a59b1f27ae2f1367c2
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-06-12 19:31:05 -0400

    Time: 13 ms (93.84%) | Memory: 18.6 MB (91.52%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,22 +1,25 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
-        dp = [[0 for _ in range(n)] for _ in range(n)]
+        dp = [0] * n
 
         for c in range(n):
-            dp[0][c] = matrix[0][c]
+            dp[c] = matrix[0][c]
         
         for r in range(1,n):
+            new_dp = [0] * n
             for c in range(n):
                 if c == 0 and n == 1:
-                    dp[r][c] = dp[r-1][c]
+                    new_dp[c] = dp[c]
                 if c == 0:
-                    dp[r][c] = min(dp[r-1][c],dp[r-1][c+1]) 
+                    new_dp[c] = min(dp[c],dp[c+1])
                 elif c == n-1:
-                    dp[r][c] = min(dp[r-1][c],dp[r-1][c-1])
+                    new_dp[c] = min(dp[c],dp[c-1])
                 else:
-                    dp[r][c] = min(dp[r-1][c],dp[r-1][c-1],dp[r-1][c+1])
+                    new_dp[c] = min(dp[c],dp[c+1],dp[c-1])
                 
-                dp[r][c] += matrix[r][c]
+                new_dp[c] += matrix[r][c]
+            
+            dp = new_dp
         
-        return min(dp[n-1])
+        return min(dp)
