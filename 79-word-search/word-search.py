457aeb79e686c413cf4d65c0761fe48f54853c86
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-07-12 17:46:29 -0400

    Time: 5803 ms (15.54%) | Memory: 17.7 MB (87.31%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,33 +1,35 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS = len(board)
+        COLS = len(board[0])
+        L = len(word)
 
-        ROWS,COLS = len(board),len(board[0])
-        path = set()
-
-        def dfs(row,col,idx):
-            if idx == len(word):
+        def backtrack(r: int, c: int, idx: int, path: set) -> bool:
+            """ """
+            if idx == L:
                 return True
-            
-            if (row < 0 or row >= ROWS or
-                col < 0 or col >= COLS or
-                board[row][col] != word[idx] or
-                (row,col) in path):
+
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS:
                 return False
-            
-            path.add((row,col))
 
-            result = (dfs(row-1,col,idx+1) or
-                      dfs(row+1,col,idx+1) or
-                      dfs(row,col+1,idx+1) or
-                      dfs(row,col-1,idx+1))
+            if (r, c) in path:
+                return False
 
-            path.remove((row,col))
-            return result
+            if board[r][c] != word[idx]:
+                return False
+
+            path.add((r, c))
+            for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+                nxt_r, nxt_c = r + dx, c + dy
+                if backtrack(nxt_r, nxt_c, idx + 1,path):
+                    return True
+                
+            path.remove((r,c))
+            return False
 
         for r in range(ROWS):
             for c in range(COLS):
-                if dfs(r,c,0):
-                    return True
-        
+                if board[r][c] == word[0]:
+                    if backtrack(r, c, 0, set()):
+                        return True
         return False
-        
