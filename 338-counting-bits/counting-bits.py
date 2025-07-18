0846b3106837f10362b3edd082c5f889d63a76a7
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-07-18 10:05:57 -0400

    Time: 4 ms (69.17%) | Memory: 18.6 MB (22.77%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -2,15 +2,7 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = [0] * (n + 1)
 
-        for i in range(n + 1):
-            ans[i] = self.countOnes(bin(i)[2:])
+        for i in range(1, n + 1):
+            ans[i] = ans[i // 2] + i % 2
 
         return ans
-
-    def countOnes(self, num: str):
-        count = 0
-        for char in num:
-            if char == "1":
-                count += 1
-
-        return count
