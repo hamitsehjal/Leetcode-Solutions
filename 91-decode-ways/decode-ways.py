bcdfe5ce3a6e15bc4ac4105fb92e0d96421760be
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-07-19 14:28:42 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.63%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,25 +1,15 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        def dp(i):
-            if i in memo:
-                return memo[i]
 
-            if i == n:
-                return 1
-            ways = 0
-            if s[i] != "0":
-                # process the single digit i.e., s[i]
-                ways += dp(i+1)
-
-                if i + 1 < n and 1 <= int(s[i:i+2]) <= 26:
-                    # process the double digit i.e., s[i:i+2] - eg 10,11, etc
-                    ways += dp(i+2)
-            
-            memo[i] = ways
-            return ways
-
-        
         n = len(s)
-        memo = {}
+        dp = [0] * (n + 1)
+        dp[n] = 1
 
-        return dp(0)
+        for i in range(n - 1, -1, -1):
+            if s[i] != "0":
+                dp[i] += dp[i + 1]
+
+                if i + 1 < n and 1 <= int(s[i : i + 2]) <= 26:
+                    dp[i] += dp[i + 2]
+
+        return dp[0]
