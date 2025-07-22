64171ca91a658d76a6739dd4415301a0d48d3352
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-07-22 18:58:51 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.42%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,18 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        """
-        Observations
-        - The robot can only move either down or right at any point in time.
-        """
+        dp = [[0 for _ in range(n)]for _ in range(m)]
 
-        dp = [[0 for _ in range(n)] for _ in range(m)]
-        dp[0][0] = 1
-
+        if m == 1 and n == 1:
+            return 1
+        
+        
+        for c in range(n):
+            dp[0][c] = 1
+        
         for r in range(m):
-            for c in range(n):
-                if r - 1 >= 0 and r - 1 < m:
-                    dp[r][c] += dp[r - 1][c]
-                if c - 1 >= 0 and c - 1 < n:
-                    dp[r][c] += dp[r][c - 1]
-
-        return dp[m - 1][n - 1]
+            dp[r][0] = 1
+        
+        for r in range(1,m):
+            for c in range(1,n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+    
+        return dp[m-1][n-1]
