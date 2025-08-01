e876ab5437996da81b408d09009a993e0698ec95
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-08-01 15:00:56 -0400

    Time: 23 ms (33.15%) | Memory: 19.2 MB (68.89%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,31 +1,33 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         """
-        How much water can be trapped at each index
-        - if we knew the hieght of the tallest bar on both left and right side of each index
-        
+        - How much water can be stored at each index?
+        - For each index, we need to calculate the highest bar on left and right of each index
         """
-        n = len(height)
-        left = [0] * len(height)
-        right = [0] * len(height)
 
+        n = len(height)
+        left = [0]*n
+        right = [0]*n
+
+        # each value corresponds to the height of the heighest bar to left of it
         max_h = height[0]
         for i in range(1,n):
             left[i] = max_h
             max_h = max(max_h,height[i])
 
+        
         max_h = height[n-1]
         for i in range(n-2,-1,-1):
             right[i] = max_h
             max_h = max(max_h,height[i])
-            
-
+        
         ans = 0
         for i in range(n):
-            l_max = left[i]
-            h = height[i]
-            r_max = right[i]
-            if l_max > h and r_max > h:
-                ans += min(r_max,l_max) - h
+            cur_h = height[i]
+            left_h = left[i]
+            right_h = right[i]
 
+            if left_h > cur_h and right_h > cur_h:
+                ans += min(left_h,right_h) - cur_h
+        
         return ans
