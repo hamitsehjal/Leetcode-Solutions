aacf640c46c39cbdf8b97233c189bb0730d3dd5e
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-08-11 14:51:39 -0400

    Time: 7 ms (87.12%) | Memory: 19.4 MB (38.31%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,46 +1,28 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        """
-        - height of the highest bar on the left
-        - height of the highest bar on the right
-
-        Approach 1:
-        - during first cycle, for each index, the find the height of highest bar on the left
-        - druing second cycle, for each index, find the height of the highest bar on the right
-
-        - for final result,
-        max_left < current height < max_right ??
-        - min(max_left,max_right) - current_height
-        - repeat the above process for each index - O(N)
-
-        - O(3N) = O(N) linear time complexity
-        - O(2N) = O(N) space for storing those left and right maxiums
-        """
-        n = len(height)
-        maxHeightsLeft = [0]*n
-        maxHeightsRight = [0]*n
-
-        cur_max = height[0]
-        for i in range(1,n):
-            maxHeightsLeft[i] = cur_max
-            cur_max = max(cur_max,height[i])
-            
+        if not height or len(height) < 3:
+            return 0
         
-
-        cur_max = height[n-1]
-        for i in range(n-2,-1,-1):
-            maxHeightsRight[i] = cur_max
-            cur_max = max(cur_max,height[i])
+        left, right = 0, len(height) - 1
+        left_max, right_max = 0, 0
+        water_trapped = 0
         
-        ans = 0
-        for i in range(n):
-            max_l = maxHeightsLeft[i]
-            max_r = maxHeightsRight[i]
-            cur_h = height[i]
-
-            if cur_h < max_l and cur_h < max_r:
-                area = min(max_l,max_r) - cur_h
-                ans += area
-
+        while left < right:
+            if height[left] < height[right]:
+                # Process left side
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    # Water can be trapped here
+                    water_trapped += left_max - height[left]
+                left += 1
+            else:
+                # Process right side
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    # Water can be trapped here
+                    water_trapped += right_max - height[right]
+                right -= 1
         
-        return ans
+        return water_trapped
