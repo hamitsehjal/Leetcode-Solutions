b98b543dd4fa72ea14e82134bf1eca5e032c0b53
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-08-25 23:45:42 -0400

    Time: 4 ms (85.50%) | Memory: 20.8 MB (89.14%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,28 +1,14 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        """
-        [[1,100],[11,22],[1,11],[2,12]]
-        [[1,100],[1,11],[2,12],[11,22]] - sorted by start time
-            - 3 removals
-        [[1,11],[2,12],[11,22],[1,100]] - sorted by end time
+        sorted_intervals = sorted(intervals,key=lambda x: x[0])
 
-        """
-        removals = 0
-        intervals.sort(key=lambda x:x[0])
         merged = []
 
-        for interval in intervals:
-            
+        for interval in sorted_intervals:
             if not merged or interval[0] > merged[-1][1]:
                 merged.append(interval)
             else:
-                # found conflict
-                removals += 1
-                cur_end_time = interval[1]
-                prev_end_time = merged[-1][1]
+                # conflict - current interval's timestamp is overlapping with previous one
+                merged[-1] = [merged[-1][0],max(merged[-1][1],interval[1])]
 
-                merged[-1][1] = max(prev_end_time,cur_end_time)
-
-        
         return merged
-        
