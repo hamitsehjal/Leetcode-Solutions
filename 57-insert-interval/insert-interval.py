8f5ac1121829950b8c239b56640de0a14a70b189
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-08-27 21:33:22 -0400

    Time: 7 ms (7.00%) | Memory: 19.9 MB (5.05%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -2,36 +2,31 @@ class Solution:
     def insert(
         self, intervals: List[List[int]], newInterval: List[int]
     ) -> List[List[int]]:
-
-        insertion_point = self.findInsertionPoint(intervals, newInterval)
-        intervals.insert(insertion_point, newInterval)
-
-        return self.mergeIntervals(intervals)
-
-    def findInsertionPoint(
-        self, intervals: List[List[int]], newInterval: List[int]
-    ) -> int:
-        left, right = 0, len(intervals) - 1
-
-        while left <= right:
-            mid = (left + right) // 2
-
-            if intervals[mid][0] == newInterval[0]:
-                return mid
-            elif intervals[mid][0] < newInterval[0]:
-                left += 1
-            else:
-                right -= 1
-
-        return left
-
-    def mergeIntervals(self, intervals: List[List[int]]) -> List[List[int]]:
         merged = []
-        for interval in intervals:
-            if not merged or interval[0] > merged[-1][1]:
-                merged.append(interval)
-            else:
-                merged[-1][0] = min(interval[0], merged[-1][0])
-                merged[-1][1] = max(interval[1], merged[-1][1])
+        n = len(intervals)
+        i = 0
+
+        # add all intervals to the list that ends before the start of newInterval
+        while i < n and intervals[i][1] < newInterval[0]:
+            merged.append(intervals[i])
+            i += 1
+
+        print(f"After Phase 1 - {merged}")
+        # merge all the overlapping intervals with the newInterval
+        # b_start < a_end
+        while i < n and intervals[i][0] <= newInterval[1] and newInterval[0] <= intervals[i][1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            print(f"Updated newInterval - {newInterval}")
+            i += 1
+
+        merged.append(newInterval)
+        print(f"After Phase 2 - {merged}")
+
+        while i < n:
+            merged.append(intervals[i])
+            i += 1
+        
+        print(f"After Phase 3 - {merged}")
 
         return merged
