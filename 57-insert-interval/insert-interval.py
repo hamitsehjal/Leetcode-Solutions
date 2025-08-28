c2cd4317a57b37d1084b85abb6a876800c59044b
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-08-27 21:36:37 -0400

    Time: 0 ms (100.00%) | Memory: 19.6 MB (89.63%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -10,23 +10,17 @@ class Solution:
         while i < n and intervals[i][1] < newInterval[0]:
             merged.append(intervals[i])
             i += 1
-
-        print(f"After Phase 1 - {merged}")
         # merge all the overlapping intervals with the newInterval
         # b_start < a_end
-        while i < n and intervals[i][0] <= newInterval[1] and newInterval[0] <= intervals[i][1]:
+        while i < n and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0], intervals[i][0])
             newInterval[1] = max(newInterval[1], intervals[i][1])
-            print(f"Updated newInterval - {newInterval}")
             i += 1
 
         merged.append(newInterval)
-        print(f"After Phase 2 - {merged}")
 
         while i < n:
             merged.append(intervals[i])
             i += 1
-        
-        print(f"After Phase 3 - {merged}")
 
         return merged
