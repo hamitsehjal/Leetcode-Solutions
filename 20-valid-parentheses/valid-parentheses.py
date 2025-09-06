ca03bab8538a2aabc697b18695ed5c3c76db2b5e
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-05 22:39:22 -0400

    Time: 7 ms (9.82%) | Memory: 12.9 MB (7.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,16 +1,62 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        brackets = {")": "(", "}": "{", "]": "["}
-
-        for ch in s:
-            if ch in brackets:
-                # closing bracket
-                if not stack or brackets[ch] != stack[-1]:
-                    return False
-
-                stack.pop()
-            else:
-                stack.append(ch)
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
         
-        return True if not stack else False
+        # must be closed
+        # order matters
+        "()[]{}" -> true
+        "([ ])" -> true
+        
+        
+        
+        "({[ ]}" -> true
+        
+        
+        # ['(' ] -> stack 
+        # ']' -> stack.pop() [
+            } {
+                stack will be empty -> we found all the opening for the closing ones
+       
+        #for (s):
+            if opening:
+                stack.add(s[i])
+            else:
+                # closing
+                if stack.pop() != s[i]:
+                    return False
+        if stack is empty:
+            return True
+        return False
+        """
+        
+        """
+         "()[]{}"
+         
+        
+        []
+        """
+        stack = []
+    
+        for i in range(len(s)):
+            if s[i] == '(' or s[i] == '{' or s[i] == '[':
+                stack.append(s[i])
+            else:
+                # It's a closing one
+                if len(stack) == 0:
+                    return False
+                else:
+                    if s[i] == ']' and stack.pop() != '[':
+                        return False
+                    elif s[i] == '}' and stack.pop() != '{':
+                        return False
+                    elif s[i] == ')' and stack.pop() != '(':
+                        return False   
+        
+        return len(stack) == 0
+            
+            
+                
+                    
+        
