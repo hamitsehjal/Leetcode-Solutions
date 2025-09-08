93bbd5f8e81aa8daa6aa619971d4672ecb8e5bba
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-08 16:43:55 -0400

    Time: 86 ms (82.42%) | Memory: 27.2 MB (76.78%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,15 +1,20 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        """
+        [73,74,75,71,69,72,76,73]
+        [0, 1, 2, 3, 4, 5, 6, 7]
 
-        n = len(temperatures)
-        stack = []  # monotonic strictly increasing
-        ans = [0] * n
+        strictly decreasing
+        """
+        ans = [0] * len(temperatures)
+        stack = [] # strictly decreasing stack
 
-        for i in range(n):
-            while stack and temperatures[stack[-1]] < temperatures[i]:
+        for idx,temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
                 stack_top = stack.pop()
-                ans[stack_top] = i-stack_top
-
-            stack.append(i)
+                ans[stack_top] = idx-stack_top
+            
+            stack.append(idx)
+        
 
         return ans
