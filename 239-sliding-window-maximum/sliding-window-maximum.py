151d8f543e5cbfa2cab68ca0f7747ea72101c6b2
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-08 23:59:28 -0400

    Time: 143 ms (91.21%) | Memory: 31.8 MB (42.80%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,18 +1,21 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        queue = deque() # monotonic decreasing
+        queue = deque() # monotonic non-increasing queue
         ans = []
 
         for i in range(len(nums)):
             while queue and nums[i] > nums[queue[-1]]:
                 queue.pop()
-            
+
             queue.append(i)
 
+            # queue[0] is the index of the maximum element
+            # if queue[0] + k == i, then it is outside the window
             if queue[0] + k == i:
                 queue.popleft()
             
+            # only add to answer if the window has reached the size k
             if i >= k-1:
                 ans.append(nums[queue[0]])
-        
+            
         return ans
