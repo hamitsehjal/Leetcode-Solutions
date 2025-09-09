959eb56749eb8beefe86df5e1ca4783f6bf0ea3e
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-09 18:29:52 -0400

    Time: 45 ms (92.53%) | Memory: 22.7 MB (71.70%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -1,12 +1,12 @@
 class StockSpanner:
 
     def __init__(self):
-        self.stack = []  # monotonically strictly decreasing
+        self.stack = []  # [idx,price]
         self.index = -1
 
     def next(self, price: int) -> int:
         self.index += 1
-        if len(self.stack) > 0:
+        if self.stack:
             while self.stack and self.stack[-1][1] <= price:
                 self.stack.pop()
 
@@ -14,11 +14,11 @@ class StockSpanner:
                 previous_greater = self.stack[-1][0]
             else:
                 previous_greater = -1
-            self.stack.append((self.index, price))
 
+            self.stack.append([self.index, price])
             return self.index - previous_greater
         else:
-            self.stack.append((self.index, price))  # (index,pair)
+            self.stack.append([self.index, price])
             return 1
 
 
