4e464aa5840293fd4d1680e956f21c4c6e6d5c4c
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-09 16:42:03 -0400

    Time: 179 ms (93.68%) | Memory: 26.2 MB (62.19%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,36 +1,27 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        max_queue = collections.deque()  # monotonically decreasing
-        min_queue = collections.deque()  # monotonically increasing
-        ans = 0
+        decreasing = deque() # track maximum
+        increasing = deque() # track minimum
 
-        l, r = 0, 0
+        l = ans = 0
 
-        while r < len(nums):
+        for r in range(len(nums)):
+            while decreasing and nums[r] > decreasing[-1]:
+                decreasing.pop()
+            
+            while increasing and nums[r] < increasing[-1]:
+                increasing.pop()
+            
+            decreasing.append(nums[r])
+            increasing.append(nums[r])
 
-            # update max queue
-            while max_queue and nums[max_queue[-1]] < nums[r]:
-                max_queue.pop()
-            max_queue.append(r)
-
-            # update min queue
-            while min_queue and nums[min_queue[-1]] > nums[r]:
-                min_queue.pop()
-            min_queue.append(r)
-
-            while (
-                max_queue
-                and min_queue
-                and abs(nums[max_queue[0]] - nums[min_queue[0]]) > limit
-            ):
-                if l == max_queue[0]:
-                    max_queue.popleft()
-                if l == min_queue[0]:
-                    min_queue.popleft()
-
+            while decreasing[0] - increasing[0] > limit:
+                if nums[l] == decreasing[0]:
+                    decreasing.popleft()
+                if nums[l] == increasing[0]:
+                    increasing.popleft()
                 l += 1
-
-            ans = max(ans, r - l + 1)
-            r += 1
+            
+            ans = max(ans,r-l+1)
 
         return ans
