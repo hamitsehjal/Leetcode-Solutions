95bbf457afe7dd07d732ff0096d834c89276d54a
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-09 17:11:32 -0400

    Time: 0 ms (100.00%) | Memory: 18.3 MB (20.94%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,22 +1,11 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = [-1] * len(nums1)
-        next_greater_element = [-1] * len(nums2)
-        stack = [] # monotonic non-increasing stack
+        stack = []
+        hashmap = {}
 
-        for i in range(len(nums2)):
-            while stack and nums2[stack[-1]] < nums2[i]:
-                stack_top = stack.pop()
-                next_greater_element[stack_top] = nums2[i]
-            
-            stack.append(i)
+        for num in nums2:
+            while stack and num > stack[-1]:
+                hashmap[stack.pop()] = num
+            stack.append(num)
         
-        indexMap = {}
-        for idx,num in enumerate(nums2):
-            indexMap[num] = idx
-        
-        for i in range(len(nums1)):
-            j = indexMap[nums1[i]]
-            ans[i] = next_greater_element[j]
-        
-        return ans
+        return [hashmap.get(i,-1) for i in nums1]
