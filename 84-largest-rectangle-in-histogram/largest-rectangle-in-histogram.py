14523b4d435ed87d24f352e46e69c357d771946d
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-10 16:21:47 -0400

    Time: 231 ms (15.43%) | Memory: 31.2 MB (45.02%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,13 +1,45 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        n = len(heights)
-        maxArea = 0
-        stack = []
 
-        for i in range(n + 1):
-            while stack and (i == n  or heights[stack[-1]] >= heights[i]):
-                height = heights[stack.pop()]
-                width = i if not stack else i - stack[-1] - 1
-                maxArea = max(maxArea, height * width)
+
+        n = len(heights)
+
+        # Find the next smaller
+        next_smaller = [n] * n
+        stack = [] # non-decreasing
+        for i in range(len(heights)):
+            while stack and heights[i] < heights[stack[-1]]:
+                stack_top = stack.pop()
+                next_smaller[stack_top] = i
+            
             stack.append(i)
-        return maxArea
+        
+
+        # Find the previous smaller
+        prev_smaller = [-1] * n
+        stack = [] # strictly increasing
+        for i in range(len(heights)):
+            while stack and heights[i] <= heights[stack[-1]]:
+                stack.pop()
+            
+            if stack:
+                prev_smaller[i] = stack[-1]
+            
+            stack.append(i)
+
+        area = 0
+
+        for i in range(len(heights)):
+            next_s = next_smaller[i]
+            prev_s = prev_smaller[i]
+
+
+            next_s = next_s - 1
+            prev_s = prev_s + 1
+
+            width = next_s - prev_s + 1
+            height = heights[i]
+
+            area = max(area,height * width)
+        
+        return area
