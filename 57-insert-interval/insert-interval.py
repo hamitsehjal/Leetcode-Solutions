4edcb7510fcf16255230067684c1f953a6fa797b
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-11 18:06:33 -0400

    Time: 3 ms (50.10%) | Memory: 19.9 MB (19.36%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -5,7 +5,8 @@ class Solution:
         """
         Three Step Process
         1. Add all intervals that start before newInterval to the merged list
-        2.
+        2. Merge all overlapping intervals with the new-interval
+        3. Add the new-interval and rest of the remaining intervals
         """
         merged = []
         i = 0
@@ -15,11 +16,7 @@ class Solution:
             merged.append(intervals[i])
             i += 1
 
-        while (
-            i < n
-            and intervals[i][0] <= newInterval[1]
-            and newInterval[0] <= intervals[i][1]
-        ):
+        while i < n and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(intervals[i][0], newInterval[0])
             newInterval[1] = max(intervals[i][1], newInterval[1])
             i += 1
