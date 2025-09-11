1ae5e03014c2526ab3d449f4df17933d2d117f96
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-11 18:03:41 -0400

    Time: 7 ms (6.86%) | Memory: 19.9 MB (19.36%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -2,19 +2,26 @@ class Solution:
     def insert(
         self, intervals: List[List[int]], newInterval: List[int]
     ) -> List[List[int]]:
+        """
+        Three Step Process
+        1. Add all intervals that start before newInterval to the merged list
+        2.
+        """
         merged = []
-        n = len(intervals)
         i = 0
+        n = len(intervals)
 
-        # add all intervals to the list that ends before the start of newInterval
         while i < n and intervals[i][1] < newInterval[0]:
             merged.append(intervals[i])
             i += 1
-        # merge all the overlapping intervals with the newInterval
-        # b_start < a_end
-        while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
+
+        while (
+            i < n
+            and intervals[i][0] <= newInterval[1]
+            and newInterval[0] <= intervals[i][1]
+        ):
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
             i += 1
 
         merged.append(newInterval)
