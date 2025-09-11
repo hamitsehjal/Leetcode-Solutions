e389118a036f82dcc4bfdcf2aab11445ff9cbf4f
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-11 18:10:04 -0400

    Time: 5 ms (9.70%) | Memory: 19.6 MB (60.11%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,13 +1,12 @@
 class Solution:
-    def insert(
-        self, intervals: List[List[int]], newInterval: List[int]
-    ) -> List[List[int]]:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         """
         Three Step Process
-        1. Add all intervals that start before newInterval to the merged list
-        2. Merge all overlapping intervals with the new-interval
-        3. Add the new-interval and rest of the remaining intervals
+        1. First add all the intervals that ends before newInterval starts
+        2. merge all overlapping intervals with newIntervals
+        3. Add the newInterval and rest of the remaining intervals
         """
+
         merged = []
         i = 0
         n = len(intervals)
@@ -15,16 +14,16 @@ class Solution:
         while i < n and intervals[i][1] < newInterval[0]:
             merged.append(intervals[i])
             i += 1
-
+        
         while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(intervals[i][0], newInterval[0])
-            newInterval[1] = max(intervals[i][1], newInterval[1])
+            newInterval[0] = min(newInterval[0],intervals[i][0])
+            newInterval[1] = max(newInterval[1],intervals[i][1])
             i += 1
-
+        
         merged.append(newInterval)
 
         while i < n:
             merged.append(intervals[i])
             i += 1
-
+    
         return merged
