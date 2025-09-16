df1b2d075df1596ed2cee4522b5ae9897cadde83
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-15 22:40:47 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (88.56%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,38 +4,35 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         if not list1:
             return list2
-        
         if not list2:
             return list1
         
-
         dummy = ListNode()
-        res = dummy
-        cur1,cur2 = list1,list2
+        cur = dummy
 
-        print(res)
-        while cur1 and cur2:
-            if cur1.val <= cur2.val:
-                res.next = ListNode(cur1.val)
-                cur1 = cur1.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
             else:
-                res.next = ListNode(cur2.val)
-                cur2 = cur2.next
+                cur.next = list2
+                list2 = list2.next
 
-            print(res)
-            res = res.next
+            cur = cur.next
 
-        while cur1:
-            res.next = ListNode(cur1.val)
-            cur1 = cur1.next
-            res = res.next
-        
-        while cur2:
-            res.next = ListNode(cur2.val)
-            cur2 = cur2.next
-            res = res.next
-        
+        while list1:
+            cur.next = list1
+            list1 = list1.next
+            cur = cur.next
+
+        while list2:
+            cur.next = list2
+            list2 = list2.next
+            cur = cur.next
+
         return dummy.next
