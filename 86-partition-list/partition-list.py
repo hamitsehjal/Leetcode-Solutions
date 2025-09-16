d20faf2cc2bf5325d8940fd7cf4cfd12d718378c
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-15 23:31:33 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.55%) - LeetSync

--- 86-partition-list/partition-list.py
+++ 86-partition-list/partition-list.py
@@ -5,56 +5,24 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        """
-        [1,4,3,2,5,2], x = 3
-        Two linked-lists
-        1. Values that are less than  x
-        2. Values greater than or equal to X
+        smallerDummy = ListNode(-101)
+        greaterThanOrEqualDummy = ListNode(101)
 
+        smaller = smallerDummy
+        greaterEqual = greaterThanOrEqualDummy
+        cur = head
 
-
-        Approach - 1
-
-        Two linked-lists
-        1. Values that are less than  x
-        2. Values greater than or equal to X    
-
-        Algorithm:
-        - iterate over the linkedList
-        - perform the comparison of val < x
-            True: add to first list
-            False: add to second list
-        
-        - add the head of second list to the next pointer of first list
-
-        Time Complexity: O(N)
-        Space Complexity: O(N)
-
-        # Test-case
-        head = 1 -> 4 -> 3 -> 2 -> 5 -> 2
-
-
-        """
-
-        smallerThan = ListNode()
-        greaterThanOrEqual = ListNode()
-
-        cur1 = smallerThan
-        cur2 = greaterThanOrEqual
-        while head:
-            if head.val < x:
-                cur1.next = head
-                cur1 = cur1.next
+        while cur:
+            if cur.val < x:
+                smaller.next = cur
+                smaller = smaller.next
             else:
-                cur2.next = head
-                cur2 = cur2.next
-                
-            head = head.next
-        
-        
-        
-        cur2.next = None
-        cur1.next = greaterThanOrEqual.next
-
-        return smallerThan.next
+                greaterEqual.next = cur
+                greaterEqual = greaterEqual.next
             
+            cur = cur.next
+        
+        greaterEqual.next = None
+        smaller.next = greaterThanOrEqualDummy.next # connecting end of smaller to start of greaterEqual
+
+        return smallerDummy.next
