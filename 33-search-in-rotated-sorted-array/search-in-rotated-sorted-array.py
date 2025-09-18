b3175fd7e4b35688bfa7e31bf7cd237ba79afefc
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-09-18 18:35:14 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (66.65%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,46 +1,38 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # find the pivot index first
-        l,r = 0,len(nums)-1
+        """
+        [5,1,3], target = 5
+        [0,1,2]
 
-        while l < r:
-            mid = l + (r-l) // 2
+        while 0 <= 2:
+            mid = 1
+            if 1 == 5:
+                ...
+            elif 5 <= 5 <= 1
+        """
+        l = 0
+        r = len(nums) - 1
 
-            if nums[mid] <= nums[-1]:
-                r = mid
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] > nums[l]:
+                # l .... mid (sorted)
+
+                if nums[l] <= target <= nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
             else:
-                l = mid + 1
-        
-        pivot = l
-
-        # decide on range based on pivot
-        if nums[0] <= target <= nums[pivot-1]:
-            # left range
-            l,r = 0,pivot-1
-
-            while l <= r:
-                mid = l + (r-l)//2
-
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    r = mid - 1
-                else:
+                # mid ... right(sorted)
+                if mid+1 < len(nums) and nums[mid+1] <= target <= nums[r]:
                     l = mid + 1
-        
-        if nums[pivot] <= target <= nums[-1]:
-            # right range
-            l,r = pivot,len(nums)-1
-
-            while l <= r:
-                mid = l + (r-l)//2
-
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    r = mid - 1
                 else:
-                    l = mid + 1
-        
+                    r = mid - 1
+            
+
         return -1
-        
+
+
