ed0f566ba4416324ef7c4ca51a09895471e4000f
Hamit Sehjal <hamitsehjal26@gmail.com>
2025-10-06 18:05:56 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.26%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,24 +1,27 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        
         cur_string = ""
-        cur_digit = ""
+        cur_count = 0
         stack = []
 
         for ch in s:
             if ch == "[":
                 stack.append(cur_string)
-                stack.append(int(cur_digit))
+                stack.append(cur_count)
+
                 cur_string = ""
-                cur_digit = ""
+                cur_count = 0
 
             elif ch == "]":
                 count = stack.pop()
                 val = stack.pop()
+
                 cur_string = cur_string * count
                 cur_string = val + cur_string
 
             elif ch.isdigit():
-                cur_digit += ch
+                cur_count = cur_count * 10 + int(ch)
             else:
                 cur_string += ch
         
